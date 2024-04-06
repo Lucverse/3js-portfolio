3d9a393ba2c68ca1e0300013c976847da4b9d52f
Ujjawal <Ujjawal.singh@theyarnbazaar.com>
2024-04-06 14:41:28 +0530

    Add @react-three/drei and @react-three/fiber dependencies, and import necessary components in App.js

---
+++ src/components/earth/Earth.jsx
@@ -0,0 +1,13 @@
+import React, { useRef } from 'react'
+import { useGLTF } from '@react-three/drei'
+
+export default function Earth(props) {
+  const { nodes, materials } = useGLTF('/earth.gltf')
+  return (
+    <group {...props} dispose={null}>
+      <mesh geometry={nodes.Object_4.geometry} material={materials['Scene_-_Root']} scale={0.6} />
+    </group>
+  )
+}
+
+useGLTF.preload('/earth.gltf')
