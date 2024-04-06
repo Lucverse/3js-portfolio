5a896a1ada269baa26dd00f4c4fb1ececab9f8c3
Ujjawal <Ujjawal.singh@theyarnbazaar.com>
2024-04-06 12:57:48 +0530

    basic layout done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import './App.css';
+import data from './data.json';
+import Username from './components/username/username';
+import BasicDetails from './components/basicDetail/basicDetail';
+import Breaker from './components/breaker/breaker';
+import Timeline from './components/timeline/timeline';
+import BubbleEffect from './components/bubbleEffect/bubbleEffect';
 
 function App() {
   return (
-    <div>
+    <div className='main-div'>
+      <Username username={data.name} />
+      <Breaker />
+      <BasicDetails BasicDetails={data.basicDetails} />
+      <Breaker/>
+      <Timeline projects={data.projects}/>
+      <BubbleEffect/>
     </div>
   );
 }
