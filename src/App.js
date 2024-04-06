67c24585dea8c33d9ecb08740a1a76bd0aa273ff
Ujjawal <Ujjawal.singh@theyarnbazaar.com>
2024-04-06 14:42:04 +0530

    Remove BubbleEffect component import from App.js and update Earth.jsx import statement

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Username from './components/username/username';
 import BasicDetails from './components/basicDetail/basicDetail';
 import Breaker from './components/breaker/breaker';
 import Timeline from './components/timeline/timeline';
-import BubbleEffect from './components/bubbleEffect/bubbleEffect';
+// import BubbleEffect from './components/bubbleEffect/bubbleEffect';
 import { Canvas } from '@react-three/fiber';
 import { ContactShadows, Environment, OrbitControls } from '@react-three/drei';
 import Earth from './components/earth/Earth';
