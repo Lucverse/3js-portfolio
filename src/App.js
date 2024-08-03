566c90a8603db0a5b94abcbb01a5d152847bb5a3
Ujjawal <Ujjawal.singh@theyarnbazaar.com>
2024-08-04 04:39:07 +0530

    add human pointed face

--- src/App.js
+++ src/App.js
@@ -4,19 +4,21 @@ import Username from './components/username/username';
 import BasicDetails from './components/basicDetail/basicDetail';
 import Breaker from './components/breaker/breaker';
 import Timeline from './components/timeline/timeline';
-import ComputersCanvas from './components/computer/Computer';
+import ThreeScene from './components/human/human';
 function App() {
   return (
     <div className='main-div'>
       <div className='first-div'>
-        <Username username={data.name} />
+        <div className='basic-info'>
+          <Username username={data.name} />
+          <BasicDetails BasicDetails={data.title} />
+        </div>
         <div className='canvas-div'>
-          <ComputersCanvas />
+          <ThreeScene />
         </div>
       </div>
       <Breaker />
-      <Breaker />      
-      <BasicDetails BasicDetails={data.basicDetails} />
+      <Breaker />
       <Breaker />
       <Timeline projects={data.projects} />
     </div>
--- src/components/username/username.jsx
+++ src/components/username/username.jsx
@@ -3,11 +3,14 @@ import React from 'react';
 const Username = ({ username }) => {
     const nameParts = username.split(' ');
     return (
-        <div className='username-div'>
-            {nameParts.map((part, index) => (
-                <p className='username' key={index}>{part}</p>
-            ))}
-        </div>
+        <>
+            <div className='username-div'>
+                {nameParts.map((part, index) => (
+                    <p className='username' key={index}>{part}</p>
+                ))}
+            </div>
+
+        </>
     );
 }
 
