f5c262cb4741bb7d9f0fe544d65b5f8576d719df
Ujjawal <Ujjawal.singh@theyarnbazaar.com>
2025-06-18 00:07:44 +0530

    feat: refactor Stars component to simplify geometry creation and improve performance

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,35 +1,38 @@
-import { useState, useRef, Suspense, useMemo } from "react";
+import { useState, useRef, Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
 import { random } from "maath";
-import * as THREE from "three";
 
 const Stars = (props) => {
   const ref = useRef();
-  const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.2 }));
 
-  for (let i = 0; i < sphere.length; i++) {
-    if (isNaN(sphere[i])) sphere[i] = 0;
-  }
-  const geometry = useMemo(() => {
-    const geo = new THREE.BufferGeometry();
-    geo.setAttribute("position", new THREE.BufferAttribute(sphere, 3));
-    return geo;
-  }, [sphere]);
+  const [positions] = useState(() => {
+    const points = new Float32Array(1500 * 3);
+    random.inSphere(points, { radius: 1.2 });
+    return points;
+  });
 
-  useFrame((state, delta) => {
-    ref.current.rotation.x -= delta / 50;
-    ref.current.rotation.y -= delta / 50;
+  useFrame((_, delta) => {
+    if (ref.current) {
+      ref.current.rotation.x -= delta / 10;
+      ref.current.rotation.y -= delta / 15;
+    }
   });
 
   return (
-    <group rotation={[0, 0, Math.PI / 2]}>
-      <Points ref={ref} geometry={geometry} frustumCulled {...props}>
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points
+        ref={ref}
+        positions={positions}
+        stride={3}
+        frustumCulled
+        {...props}
+      >
         <PointMaterial
           transparent
-          color='#f272c8'
+          color="#f272c8"
           size={0.002}
-          sizeAttenuation={true}
+          sizeAttenuation
           depthWrite={false}
         />
       </Points>
