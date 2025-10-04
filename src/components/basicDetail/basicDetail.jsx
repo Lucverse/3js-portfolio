1bd8c2d08459146d7ed345a99d6d4076a8be9a54
Ujjawal <Ujjawal.singh@theyarnbazaar.com>
2025-10-04 22:41:13 +0530

    fix: update BasicDetails component to correctly render title and address

--- src/components/basicDetail/basicDetail.jsx
+++ src/components/basicDetail/basicDetail.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const BasicDetails = ({ BasicDetails }) => {
     return (
         <div className='basic-details-div'>
-            <p className='basic-details'>{BasicDetails}</p>
+            <p className='basic-details'>{BasicDetails.title}</p>
+            <p className='basic-address'>{BasicDetails.address.city}, {BasicDetails.address.country}</p>
         </div>
     );
 }
